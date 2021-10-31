36a310a3dc97dd15eb954e138e5e8bd201a672fe
fathurzoy <fathurrahmanf123@gmail.com>
2021-10-31 20:05:00 +0700

    splash screen

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {SplashScreen} from './pages';
+
+const App = () => {
+  return (
+    <>
+      <SplashScreen />
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
