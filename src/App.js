0975f6afa8948a129848d2f49d3c60459a0dd3dd
fathurzoy <fathurrahmanf123@gmail.com>
2021-11-20 15:59:59 +0700

    Setup Redux

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import {SignIn, SplashScreen} from './pages';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './router';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
   return (
     <>
       <NavigationContainer>
-        {/* Rest of your app code */}
-        {/* <SplashScreen /> */}
-        <Router />
+        <Provider store={store}>
+          {/* Rest of your app code */}
+          {/* <SplashScreen /> */}
+          <Router />
+        </Provider>
       </NavigationContainer>
     </>
   );
